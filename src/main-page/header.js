3a5431134a307183ca4f12e6f02be4ddca799282
Gerardo Luis <gerardo.luism@gmail.com>
2023-07-18 19:42:42 -0600

    Using class instead of functions

--- src/main-page/header.js
+++ src/main-page/header.js
@@ -1,16 +1,20 @@
+import { Component } from "react";
 import logo from "./logo.png";
 
-const Header = ( {title, subtitle} ) => (
-  <header className="row">
-    <div className="col-md-5">
-      <img src={logo} className="logo" alt="logo"/>
-    </div>
-    <div className="col-md-7 mt-5 subtitle">
-      <h1>{title}</h1>
-      
-      {subtitle}
-    </div>
-  </header>
-);
+class Header extends Component {
+  render() {
+    return (
+      <header className="row">
+        <div className="col-md-5">
+          <img src={logo} className="logo" alt="logo" />
+        </div>
+        <div className="col-md-7 mt-5 subtitle">
+          <h1>{this.props.title}</h1>
+          {this.props.subtitle}
+        </div> 
+      </header>
+    );
+  }
+}
 
 export default Header;

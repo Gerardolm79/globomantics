c18a974eb6ac751dc1be2a7ffac57c7f1e96ea2d
Gerardo Luis <gerardo.luism@gmail.com>
2023-07-18 19:37:05 -0600

    Another way to manage props

--- src/main-page/header.js
+++ src/main-page/header.js
@@ -1,14 +1,14 @@
 import logo from "./logo.png";
 
-const Header = (props) => (
+const Header = ( {title, subtitle} ) => (
   <header className="row">
     <div className="col-md-5">
       <img src={logo} className="logo" alt="logo"/>
     </div>
     <div className="col-md-7 mt-5 subtitle">
-      <h1>{props.title}</h1>
+      <h1>{title}</h1>
       
-      {props.subtitle}
+      {subtitle}
     </div>
   </header>
 );
